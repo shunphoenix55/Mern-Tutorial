6b91151b280ab0f69f06263b2faab84e67e2cb7c
shunphoenix55 <76583147+shunphoenix55@users.noreply.github.com>
2023-01-03 22:59:49 +0530

    Set up React

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+// BrowserRouter keeps UI in sync with URL
+// it will only reload/refresh the component that needs to be changed
+// instead of refreshing/reloading the entire page
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
